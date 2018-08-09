9ec41160b689cfed177eda45dab6f8e1b61afcd4
Alexander Belyshev <32143234+avbelyshev@users.noreply.github.com>
2018-08-09 22:46:55 +0300

    Ajax. Part1 (#4)
    * Install gem jquery-rails
    
    * Create answer with ajax
    
    * Create answer with ajax. Without re-rendering the entire list of answers

--- spec/acceptance/answer/create_answer_spec.rb
+++ spec/acceptance/answer/create_answer_spec.rb
@@ -8,18 +8,20 @@ feature 'Create answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  scenario 'Authenticated user creates answer with valid attributes' do
+  scenario 'Authenticated user creates answer with valid attributes', js: true do
     sign_in(user)
 
     visit question_path(question)
-    fill_in 'Body', with: 'Answer'
+    fill_in 'Your answer', with: 'Answer'
     click_on 'Reply'
 
-    expect(page).to have_content 'Your answer successfully created.'
-    expect(page).to have_content 'Answer'
+    expect(current_path).to eq question_path(question)
+    within '.answers' do
+      expect(page).to have_content 'Answer'
+    end
   end
 
-  scenario 'Authenticated user creates answer with invalid attributes' do
+  scenario 'Authenticated user creates answer with invalid attributes', js: true do
     sign_in(user)
 
     visit question_path(question)
@@ -30,7 +32,7 @@ feature 'Create answer', %q{
 
   scenario 'Non-authenticated user tries to create answer' do
     visit question_path(question)
-    fill_in 'Body', with: 'Answer'
+    fill_in 'Your answer', with: 'Answer'
     click_on 'Reply'
 
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
