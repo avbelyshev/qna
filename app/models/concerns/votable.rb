17dda6041c6e605ede25f69f0538ea8adab329c8
avbelyshev <avbelishev@gmail.com>
2018-08-29 01:41:37 +0300

    Add vote model

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,29 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable, dependent: :destroy
+  end
+
+  def set_like!(user)
+    votes.create!(user: user, value: 1) if can_vote?(user)
+  end
+
+  def set_dislike!(user)
+    votes.create!(user: user, value: -1) if can_vote?(user)
+  end
+
+  def cancel_vote!(user)
+    votes.where(user: user).delete_all
+  end
+
+  def rating
+    votes.sum(:value)
+  end
+
+  private
+
+  def can_vote?(user)
+    !user.author_of?(self) && !votes.exists?(user: user)
+  end
+end
