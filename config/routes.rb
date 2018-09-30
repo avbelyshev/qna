677c65ca9194b44833d1c81d4549b2176136061a
avbelyshev <avbelishev@gmail.com>
2018-09-30 13:20:06 +0300

    Add Profiles API

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,12 @@ Rails.application.routes.draw do
 
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
+  end
 end
