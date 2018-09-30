331c3f731eba2aef08004bc30e7e1fa7f770704a
avbelyshev <avbelishev@gmail.com>
2018-09-30 13:54:43 +0300

    Add authorization to ProfilesController

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProfilesController < ApplicationController
-  skip_authorization_check
-
   before_action :doorkeeper_authorize!
+
+  authorize_resource class: User
 
   respond_to :json
 
@@ -13,5 +13,9 @@ class Api::V1::ProfilesController < ApplicationController
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @ability ||= Ability.new(current_resource_owner)
   end
 end
