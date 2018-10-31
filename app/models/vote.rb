8d3eb8749023aac2a2f1e366de2d8819adc4b4e9
avbelyshev <avbelishev@gmail.com>
2018-11-01 01:28:06 +0300

    Caching for question page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
-  belongs_to :votable, polymorphic: true, optional: true
+  belongs_to :votable, polymorphic: true, optional: true, touch: true
 
   validates :value, presence: true, inclusion: [1, -1]
 end
